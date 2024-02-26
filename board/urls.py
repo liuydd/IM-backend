e7df3ba2646b287e675319bece74ed14c68d9b37
abmfy <wangbw21@mails.tsinghua.edu.cn>
2024-02-26 20:21:46 +0800

    feat: init

---
+++ board/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+import board.views as views
+
+urlpatterns = [
+    path('startup', views.startup),
+    path('login', views.login),
+    path('boards', views.boards),
+    # TODO Start: [Student] add routing paths for `boards/<index>` and `user/<userName>`
+
+    # TODO End: [Student] add routing paths for `boards/<index>` and `user/<userName>`
+
+]
