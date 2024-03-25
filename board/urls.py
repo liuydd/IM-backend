581cbac652500568c693bde70532139a5709d539
王伊荷 <2022010751@admin.secoder.net>
2024-03-25 12:08:27 +0000

    Replace urls.py

--- board/urls.py
+++ board/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-import board.views as views
+from . import views
 
 urlpatterns = [
     path('startup', views.startup),
     path('login', views.login),
     path('boards', views.boards),
     # TODO Start: [Student] add routing paths for `boards/<index>` and `user/<userName>`
-
+    path('boards/<index>', views.boards_index),
+    path('user/<username>', views.user_board)
     # TODO End: [Student] add routing paths for `boards/<index>` and `user/<userName>`
-
 ]
