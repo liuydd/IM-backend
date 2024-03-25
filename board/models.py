97304f00e9a32b0561b7baecd1bd966fe29c1fc7
王伊荷 <2022010751@admin.secoder.net>
2024-03-25 12:06:52 +0000

    Replace models.py

--- board/models.py
+++ board/models.py
@@ -31,13 +31,15 @@ class User(models.Model):
 
 class Board(models.Model):
     # TODO Start: [Student] Finish the model of Board
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    board_state = models.CharField(max_length=MAX_CHAR_LENGTH)
+    board_name = models.CharField(max_length=MAX_CHAR_LENGTH)
+    created_time = models.FloatField(default=utils_time.get_timestamp)
     
-    # id, BigAutoField, primary_key=True
-    # user, ForeignKey to User, CASCADE deletion
-    # board_state, CharField
-    # board_name, CharField
-    # created_time, FloatField, default=utils_time.get_timestamp
-    
+    class Meta:
+        indexes = [models.Index(fields=["board_name"])]
+        unique_together = ("user", "board_name")
     # Meta data
     # Create index on board_name
     # Create unique_together on user and board_name
