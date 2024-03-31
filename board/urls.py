5da1a77e1d2b8e3b8bb13f202e36e4da43a2136a
王伊荷 <2022010751@admin.secoder.net>
2024-03-31 01:26:33 +0000

    增加了注册(register)url，对应register视图

--- board/urls.py
+++ board/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('startup', views.startup),
+    path('register', views.register),
     path('login', views.login),
     path('boards', views.boards),
     # TODO Start: [Student] add routing paths for `boards/<index>` and `user/<userName>`
