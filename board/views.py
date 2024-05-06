385eca6541ddd7305677fa0b91ec47051490308d
liuydd <1594807461@qq.com>
2024-05-06 15:26:20 +0800

    好友列表时将每个friend的userid传给前端 #14

--- board/views.py
+++ board/views.py
@@ -160,7 +160,7 @@ def list_friend(req: HttpRequest):
     return request_success({
         "code": 0,
         "info": "Succeed",
-        "friendList": [return_field(friendship.serialize(), ["friend", "labels"]) for friendship in friendships]
+        "friendList": [return_field(friendship.serialize(), ["friendid", "friend", "labels"]) for friendship in friendships]
     })
     
     
