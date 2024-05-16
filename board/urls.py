eaafc732c3877944d8933a180638883adaf83dd8
王伊荷 <2022010751@admin.secoder.net>
2024-05-16 11:30:44 +0000

    add messaging url #18

--- board/urls.py
+++ board/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('group/list_announcement', views.list_announcement),
     path('group/invitation/send', views.send_invitation),
     path('group/invitation/process', views.process_invitation),
-    path('group/invitation/get', views.get_invitation)
+    path('group/invitation/get', views.get_invitation),
+    path('messages', views.messages),
+    path('conversations', views.conversations)
 ]
